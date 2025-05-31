1d32b15caf588c45d1d22ab437aad3dfc6e25aaf
yuehan7788 <yueh80200@gmail.com>
2025-05-31 22:06:42 +0800

    Update VPS-JB.sh

--- VPS-JB.sh
+++ VPS-JB.sh
@@ -353,9 +353,9 @@ expect {
         sleep 2
         exp_continue
     }
-    "请选择:" {
-        # 根据上下文判断是哪个选择
-        if {[string match "*功能 1/1 : 选择核心安装*" $expect_out(buffer)] || [string match "*2.sing-box*" $expect_out(buffer)]} {
+    "==============================================================\r\n请选择:" {
+        # 检查是否在核心选择菜单中
+        if {[string match "*功能 1/1 : 选择核心安装*" $expect_out(buffer)]} {
             send "2\r"  # 选择sing-box
         } else {
             send "1\r"  # 选择安装
